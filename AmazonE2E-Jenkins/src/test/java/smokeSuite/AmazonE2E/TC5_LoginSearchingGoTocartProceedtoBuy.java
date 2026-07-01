83d7719414a473e69eca8a159cb3c5ec48c76feb
Geeta <geeta.meduri@gmail.com>
2026-07-01 12:07:17 +0530

    pusing the files

---
+++ AmazonE2E-Jenkins/src/test/java/smokeSuite/AmazonE2E/TC5_LoginSearchingGoTocartProceedtoBuy.java
@@ -0,0 +1,60 @@
+package smokeSuite.AmazonE2E;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import utility.BaseTest;
+import utility.RetryLogic;
+
+public class TC5_LoginSearchingGoTocartProceedtoBuy extends BaseTest
+{
+	@Test(retryAnalyzer=RetryLogic.class)
+	public void logintoGotocart() throws InterruptedException
+	{
+		Reporter.log("Browser launched successfully");
+		   HomePage homepage = new HomePage(driver);
+		   homepage.hoveroverOnAccountList(driver);
+		   homepage.clickingOnSignin();
+		   
+		   LoginPage loginpage = new LoginPage(driver);
+		   loginpage.enterUsername();
+		   loginpage.clickonContinue();
+		   loginpage.enterPassword();
+		   loginpage.clickonSignin();
+		   Reporter.log("Login is done successfully");
+		   
+		   homepage.searchingtheproduct();
+		   Reporter.log("Searching the product");
+		   
+		   SearchResultPage searchresultpage = new SearchResultPage(driver);
+		   searchresultpage.clickingOn1stProduct(driver);
+		   Reporter.log("Clicking on the FirstProduct");
+		   
+		   ProductDetailsPage productdetailspage = new ProductDetailsPage(driver);
+		   productdetailspage.clickonWishList();
+		   Reporter.log("Clicking on the wishlist");
+		   
+		   productdetailspage.clickingOnContinueShopping();
+		   productdetailspage.clickingOnAddtoCart();
+		   Reporter.log("Product successfully added to the cart");
+		 
+		   CartDetailsPage cartdetailspage = new CartDetailsPage(driver);
+		   cartdetailspage.clickingongotocart();
+		   Reporter.log("Clicking on go to cart");
+		   
+		   cartdetailspage.clickingonproceedtobuy();
+		   Reporter.log("Clicking on the proceed to buy");
+		   
+		   Assert.assertEquals(cartdetailspage.verifytheTextShoppingcart(), "Shopping Cart","Sorry conditions did not match");
+		   Reporter.log("Test case is pass");
+	
+		   
+		   
+	}
+		   
+		   
+	   
+}
+
+
